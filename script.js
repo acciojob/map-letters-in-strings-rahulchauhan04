fd5f335999f0c80f3afcf4c60bc32f116a1d594c
Rahul Chauhan <56860601+rahulchauhan04@users.noreply.github.com>
2024-05-22 20:16:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+function mapLetters(str) {
+const obj = {};
+
+for (let i = 0; i < str.length; i++) {
+	const char = str[i];
+
+	if (/[a-z]/.test(char)) {
+		if (!obj(char)) {
+			obj[char] = [];
+		}
+	}
+}
+	 return obj;
+}
